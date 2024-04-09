9c1d718de4d27ae677a9c7cad64be235f71c3540
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-04-09 20:15:23 +0200

    Added option to find height

--- binarytree.js
+++ binarytree.js
@@ -1,9 +1,3 @@
-// Set The middle element of the array as root.
-// Recursively do the same for the left half and right half.
-//      Get the middle of the left half and make it the left child of the root created in step 1.
-//      Get the middle of the right half and make it the right child of the root created in step 1.
-// Print the preorder of the tree.
-
 // only used to make the printed binary tree easier to visualize
 function prettyPrint(node, prefix = "", isLeft = true) {
   if (node === null) {
@@ -68,6 +62,11 @@ function sortArray(input) {
   }
   console.log(duplicateFreeArray);
   return duplicateFreeArray;
+}
+
+// useless callback that does nothing
+function callbackFn(node) {
+  return node;
 }
 
 // tree constructor function
@@ -197,28 +196,169 @@ const tree = (input) => {
       console.log("we go right");
       return find(val, targetNode.right);
     }
-    console.log("target found");
-    console.log(targetNode);
+    // console.log("target found");
+    // console.log(targetNode);
     return targetNode;
+  };
+
+  // breadth first
+  // level-order traversal
+  // need array acting as queue - FIFO
+  // each iteration, you add both children of your target node to end of array
+  // add value to another array holding all LOT values
+  // shift queue array by one spot and repeat until queue array length is zero - i.e no items remaining
+  // the question is asking to pass a callback function as an argument
+  // callback function does nothing, only here because it had to be included (???????)
+
+  const levelOrder = (callbackFn) => {
+    let queue = [];
+    queue.push(root.left);
+    queue.push(root.right);
+    let levelOrderArray = [];
+    levelOrderArray.push(root.value);
+
+    while (queue.length != 0) {
+      let currNode = queue[0];
+      if (callbackFn) {
+        callbackFn(currNode);
+      } else {
+        levelOrderArray.push(currNode.value);
+
+        if (currNode.left != null) {
+          queue.push(currNode.left);
+        }
+
+        if (currNode.right != null) {
+          queue.push(currNode.right);
+        }
+        queue.shift();
+      }
+    }
+    console.log(levelOrderArray);
+    return levelOrderArray;
+  };
+
+  // depth first
+  // preorder Root -> Left subtree -> Right subtree
+
+  const preorder = (node, callbackFn, inputArr) => {
+    let arr = [];
+    if (inputArr) {
+      arr = inputArr;
+    }
+
+    if (node === null) {
+      return node;
+    } else {
+      if (callbackFn) {
+        callbackFn(node);
+      } else {
+        arr.push(node.value);
+        preorder(node.left, null, arr);
+        preorder(node.right, null, arr);
+      }
+    }
+    return arr;
+  };
+  // inorder Left subtree -> Root -> Right subtree
+  const inorder = (node, callbackFn, inputArr) => {
+    let arr = [];
+    if (inputArr) {
+      arr = inputArr;
+    }
+
+    if (node === null) {
+      return node;
+    } else {
+      if (callbackFn) {
+        callbackFn(node);
+      } else {
+        inorder(node.left, null, arr);
+        arr.push(node.value);
+        inorder(node.right, null, arr);
+      }
+    }
+
+    return arr;
+  };
+  // postorder Left subtree -> Right subtree -> Root
+  const postorder = (node, callbackFn, inputArr) => {
+    let arr = [];
+    if (inputArr) {
+      arr = inputArr;
+    }
+
+    if (node === null) {
+      return node;
+    } else {
+      if (callbackFn) {
+        callbackFn(node);
+      } else {
+        postorder(node.left, null, arr);
+        postorder(node.right, null, arr);
+        arr.push(node.value);
+      }
+    }
+    return arr;
+  };
+
+  // height is the longest path from target node to leaf node
+  const height = (node, counter, arr) => {
+    let holdArr = [];
+    let tall = 0;
+
+    if (counter) {
+      tall = counter;
+    }
+
+    if (arr) {
+      holdArr = arr;
+    }
+
+    if (node === null) {
+      return node;
+    }
+
+    // if left & right is null, we found the leaf node
+    if (node.left === null && node.right === null) {
+      holdArr.push(tall);
+      // sort the array to find the longest path
+      holdArr.sort((a, b) => {
+        return b - a;
+      });
+    } else {
+      tall += 1;
+    }
+
+    height(node.left, tall, holdArr);
+    height(node.right, tall, holdArr);
+
+    // returns first item in reverse-sorted array
+    return holdArr[0];
   };
 
   return {
     root,
+    height,
+    postorder,
+    inorder,
+    preorder,
+    levelOrder,
     find,
     insert,
     deleteItem,
   };
 };
 
-let x = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 243, 3, 99, 23, 1231];
+let x = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 let testTree = tree(sortArray(x));
 // console.log(buildTree(x));
 
-// console.log(testTree.insert(14));
-// console.log(testTree.insert(18));
-// console.log(testTree.insert(123));
-// console.log(testTree.insert(2323));
+console.log(testTree.insert(14));
+console.log(testTree.insert(18));
+console.log(testTree.insert(123));
+console.log(testTree.insert(2323));
 // testTree.deleteItem(4);
 // testTree.deleteItem(7);
 // testTree.deleteItem(9);
@@ -227,4 +367,14 @@ let testTree = tree(sortArray(x));
 // console.log(testTree.root.right);
 
 prettyPrint(testTree.root);
-testTree.find(69, testTree.root);
+testTree.levelOrder();
+
+testTree.height(testTree.find(8, testTree.root));
+console.log(testTree.height(testTree.find(8, testTree.root)));
+
+// console.log(testTree.preorder(testTree.root));
+// console.log(testTree.inorder(testTree.root));
+// console.log(testTree.postorder(testTree.root));
+// testTree.preorder(testTree.root);
+
+// testTree.find(69, testTree.root);
